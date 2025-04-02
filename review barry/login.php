f46ef15a2bee9ddbda5c108eb16942262f4da377
xavidesjaakie <kakerkak11@gmail.com>
2025-04-02 11:41:50 +0200

    kaas

---
+++ review barry/login.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="nl">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Inloggen</title>
+</head>
+<body>
+    <h2>Inloggen</h2>
+    <form action="verwerk_login.php" method="post">
+        <label for="email">E-mail:</label>
+        <input type="email" id="email" name="email" required><br><br>
+
+        <button type="submit">Inloggen</button>
+    </form>
+</body>
+</html>
